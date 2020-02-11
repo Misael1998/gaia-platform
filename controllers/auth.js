be78568b9ce309efb3e5f30a3f60f426185d6735
Misael Landero <misalande@gmail.com>
2020-02-11 10:04:38 -0600

    auth login functional, with jwt response. Remaining: sp or function to get user role and refrest token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,106 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const mssql = require("mssql");
+
 //@desc     AUTH user
-//@route    GET     /api/auth
+//@route    GET     /api/auth/login
 //@access   Public
-exports.authUser = (req, res, next) => {
-  res.status(200).json({
-    success: true,
-    msg: "endpoint to auth user"
-  });
+exports.login = async (req, res, next) => {
+  let payload = {};
+  const { email, password } = req.body;
+  const { ADMIN_PASSWORD, ADMIN_EMAIL } = process.env;
+
+  try {
+    const query = await new mssql.Request()
+      .input("email", mssql.VarChar(100), email)
+      .query("select * from TBL_USERS where email = @email");
+
+    const { recordset } = query;
+    const dataExist = recordset.length > 0;
+
+    if (!dataExist) {
+      return res.status(404).json({
+        success: false,
+        msg: "Invalid credentials"
+      });
+    }
+
+    const user = query.recordset[0];
+
+    const isAdmin =
+      user.email === ADMIN_EMAIL && user.password === ADMIN_PASSWORD;
+
+    if (isAdmin) {
+      payload = {
+        id: user.idUser,
+        role: "admin"
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_KEY,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+
+          return res.status(200).json({
+            success: true,
+            token: token,
+            user: {
+              firstName: user.name,
+              lastName: user.lastname,
+              email: user.email,
+              phone: user.phone,
+              address: user.address
+            }
+          });
+        }
+      );
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(404).json({
+        success: false,
+        msg: "Invalid credentials"
+      });
+    } else {
+      // require sp that gets user role
+      payload = {
+        id: user.idUser,
+        role: ""
+      };
+
+      jwt.sign(
+        payload,
+        process.env.JWT_KEY,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+
+          return res.status(200).json({
+            success: true,
+            token: token,
+            user: {
+              firstName: user.name,
+              lastName: user.lastname,
+              email: user.email,
+              phone: user.phone,
+              address: user.address
+            }
+          });
+        }
+      );
+    }
+
+    // console.log(query);
+    // console.log(user);
+    //
+    // res.status(200).json({
+    //   success: true,
+    //   msg: "auth route"
+    // });
+  } catch (err) {
+    console.log(err);
+  }
 };
