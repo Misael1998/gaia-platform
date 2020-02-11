341599540db19a52d40e02b87ee89d99819f8433
Angel <abulnes53@gmail.com>
2020-02-11 15:52:06 -0600

    Arreglado responsive de navbar

--- client/src/views/Home/components/NavbarHome.js
+++ client/src/views/Home/components/NavbarHome.js
@@ -4,31 +4,31 @@ import { Link } from 'react-router-dom';
 const NavbarHome = ({ toSection, about, ourProducts, contact }) => {
     return (
         <nav className="navbar  w-100 navbar-light text-center primary-color">
-            <div className='col-3'>
+            <div className='col-lg-3'>
                 <button className='btn nav-links' onClick={() => toSection(about)}>
                     ¿Quienes somos?
                 </button>
             </div>
 
-            <div className='col-3'>
+            <div className='col-lg-3'>
                 <button className='btn nav-links' onClick={() => toSection(ourProducts)}>
                     Nuestros productos
                 </button>
             </div>
 
-            <div className='col-2'>
+            <div className='col-lg-2'>
                 <button className='btn nav-links' onClick={()=> toSection(contact)}>
                     Contáctanos
                 </button>
             </div>
 
-            <div className='col-2'>
+            <div className='col-lg-2'>
                 <Link className='btn nav-links' to='/'>
                     Registrate
                 </Link>
             </div>
 
-            <div className='col-2'>
+            <div className='col-lg-2'>
                 <Link className='btn nav-links' to='/'>
                     Login
                 </Link>
