a8189c0386ffb716fc90054111cc05f22b685e43
Ariadna Perez <ariadna.perez97@yahoo.com>
2020-02-16 13:36:21 -0600

    register individual client functional

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,6 @@
 const mssql = require("mssql");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 //@desc     Register individual user
 //@route    POST     /api/user/registerindivualclient
@@ -36,26 +37,51 @@ exports.registerIndividualClient = async (req, res, next) => {
     });
   }
 
+  console.log(birthDate);
+
   try {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
     const query = await new mssql.Request()
-      .input("email", mssql.VarChar(100), email)
-      .input("password", mssql.VarChar(100), password)
-      .input("phone", mssql.VarChar(12), phone)
-      .input("address", mssql.VarChar(150), address)
-      .input("name", mssql.VarChar(45), name)
-      .input("lastName", mssql.VarChar(45), lastName)
-      .input("id", mssql.VarChar(13), id)
-      .input("birthDate", mssql.Date, birthDate)
-      .output("status", mssql.VarChar(mssql.MAX))
-      .output("idUser", mssql.Int)
-      .execute("SP_REGISTER");
+      .input("email", mssql.NVarChar(100), email)
+      .input("password", mssql.NVarChar(100), hashPassword)
+      .input("phone", mssql.NVarChar(12), phone)
+      .input("address", mssql.NVarChar(150), address)
+      .input("name", mssql.NVarChar(45), name)
+      .input("lastName", mssql.NVarChar(45), lastName)
+      .input("birth_date", mssql.VarChar(45), birthDate)
+      .input("register_id", mssql.NVarChar(14), id)
+      .output("msjTemp", mssql.NVarChar(100))
+      .output("id_user", mssql.Int)
+      .execute("SP_ADD_USER_INDIVIDUAL");
 
-    const { status, idUser } = query.output;
-
-    if ((status = 1)) {
-      console.log(1);
+    console.log(query);
+    const { id_user, msjTemp } = query.output;
+    if (!id_user) {
+      return res.status(400).json({
+        success: false,
+        msg: "Can't add user"
+      });
     }
+
+    const user = {
+      name: name,
+      lastname: lastName,
+      role: "individual",
+      email,
+      phone,
+      address
+    };
+
+    payload = {
+      id: id_user,
+      role: "individual"
+    };
+
+    return sendTokenResponse(user, payload, 201, res);
   } catch (err) {
+    console.log(err);
     return res.status(500).json({
       success: false,
       msg: "server error"
@@ -66,4 +92,29 @@ exports.registerIndividualClient = async (req, res, next) => {
     success: true,
     msg: "register client route"
   });
+};
+
+//send response
+const sendTokenResponse = (user, payload, statusCode, res) => {
+  jwt.sign(
+    payload,
+    process.env.JWT_KEY,
+    { expiresIn: process.env.JWT_EXPIRE },
+    (err, token) => {
+      if (err) throw err;
+
+      return res.status(200).json({
+        success: true,
+        token: token,
+        user: {
+          firstName: user.name,
+          lastName: user.lastname,
+          role: user.role,
+          email: user.email,
+          phone: user.phone,
+          address: user.address
+        }
+      });
+    }
+  );
 };
