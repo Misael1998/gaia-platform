edb70bdf807675c6e12b980a1e08ac6168514882
ledys.santos <estefany.santos129@gmial.com>
2020-02-17 01:03:23 -0600

    sp of add_individualUSER and add_enterpriseUSER with codeState (successful or unsuccessful)

--- sql/SP_PYFLOR/SP_ADD_USER_ENTERPRISE.sql
+++ sql/SP_PYFLOR/SP_ADD_USER_ENTERPRISE.sql
@@ -3,8 +3,6 @@ CREATE PROCEDURE SP_ADD_USER_ENTERPRISE(
     @password nvarchar(100),
     @phone nvarchar(12),
     @address nvarchar(150),
-    @name nvarchar(45),
-    @lastname nvarchar(45),
     @company_name nvarchar(45),
     @contact_name nvarchar(45),
     @rtn nvarchar(12),
@@ -12,81 +10,102 @@ CREATE PROCEDURE SP_ADD_USER_ENTERPRISE(
     @idCompanyType int,
     @idSector int,
     @business_name nvarchar(45),
-	@msjTemp nvarchar(100) out,
-    @id_user int out
+	@pcMsj nvarchar(100) out,
+    @id_user int out,
+    @CodeState int out /*codeState:  0=NO EXITOSO, 1=EXITOSO, 2=no existe valor o vacio, 3=ya existe valor */
     )       
 AS
 BEGIN
 DECLARE
     @VNcount int,
     @VNid_User int;
-    set @VNcount = 0;
-    set @msjTemp=' ';
+    SET @VNcount = 0;
+    SET @pcMsj=' ';
+   
 
 /* Validation of fields EMPTY when ADD user*/
-/* nombreEmpr,contact,rtn,correo,contraseña, telefono, razon social,direccion*/
-            IF @name='  ' OR @name IS NULL BEGIN 
-                SET @msjTemp='EMPTY FIELD';
-                RETURN;   
-            END;  
+/* nombreEmpr,contact_name,rtn,correo,contraseña, telefono, razon social,direccion OBLIGLATORY*/
             IF @contact_name=' ' OR @contact_name IS NULL BEGIN
-                SET @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;     
             END;               
             IF @phone =' ' OR @phone IS NULL BEGIN
-                SET @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
             IF @email=' ' OR @email IS NULL BEGIN
-                SET @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;     
             END;         
             IF @password=' ' OR @password IS NULL BEGIN 
-                set @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
             IF @address=' ' OR @address IS NULL BEGIN 
-                set @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
            
             IF @company_name=' ' OR @company_name IS NULL BEGIN 
-                set @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
 
             IF @rtn=' ' OR @rtn IS NULL BEGIN 
-                set @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
 
             IF @business_name=' ' OR @business_name IS NULL BEGIN 
-                set @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
+                RETURN;
+            END;
+
+            IF @pcMsj <>' ' BEGIN
+                SET @pcMsj= 'Error validation of input parameters';
+                SET @CodeState=0;
                 RETURN;
             END;
 
             SET @VNcount = (SELECT count(*)  FROM TBL_ENTERPRISE_CLIENTS 
             where rtn=@rtn)
             IF @VNcount>0 BEGIN
-                set @msjTemp='Existing User';
+                SET @pcMsj='Existing User';
+                SET @CodeState=3;
                 RETURN;
             END;
+
      
             INSERT INTO TBL_USERS (email,password,phone,address,name,lastname,resetPasswordToken,resetPasswordExpire)
-            VALUES(@email,@password,@phone,@address,@name,@lastname,'s',null);
+            VALUES(@email,@password,@phone,@address,@company_name,null,null,null);
             
-            SET @VNid_User =(SELECT MAX(idUser) From TBL_USERS where @email=email );
+            SET @VNid_User =(SELECT MAX(idUser) From TBL_USERS where @email=email);
 
             INSERT INTO TBL_ENTERPRISE_CLIENTS(company_name ,contact_name,rtn,contact_number,idUser,idCompanyType,idSector,business_name) 
             VALUES (@company_name,@contact_name,@rtn,@contact_number,@VNid_User,@idCompanyType,@idSector,@business_name);
             
-            SET @msjTemp='Properly Registered User';
-            set @id_user=@VNid_User;
+            IF  @VNid_User IS NULL BEGIN
+                SET @pcMsj='User Registration Error';
+                SET @CodeState=0;
+            END
+            ELSE BEGIN 
+                SET @pcMsj='Properly Registered User';
+                SET @id_user=@VNid_User;
+                SET @CodeState=1;
+            END;
 
 END;
 
 
-drop procedure SP_ADD_USER_ENTERPRISE
+/*drop procedure SP_ADD_USER_ENTERPRISE
 
 exec SP_ADD_USER_ENTERPRISE 'juan@com', 'pajarito2','9821','col','pizza','hut','PIZZAHUT','juanLopez','0122','9888','1','1','cualq','bien','8';
 
@@ -103,4 +122,4 @@ insert into TBL_COMPANY_TYPE values ('hoteles');
 insert into TBL_COMPANY_TYPE values ('restaurante');
 
 insert into TBL_SECTORS VALUES ('gourmet');
-insert into TBL_SECTORS VALUES ('vinos');
+insert into TBL_SECTORS VALUES ('vinos');*/
--- sql/SP_PYFLOR/SP_ADD_USER_INDIVIDUAL.sql
+++ sql/SP_PYFLOR/SP_ADD_USER_INDIVIDUAL.sql
@@ -7,8 +7,9 @@ CREATE PROCEDURE SP_ADD_USER_INDIVIDUAL(
     @lastname nvarchar(45),
     @birth_date varchar(45),
     @register_id nvarchar(13),
-	@msjTemp nvarchar(100) out,
-    @id_user int out
+    @pcMsj nvarchar(100) out,
+    @id_user int out,
+    @CodeState int out /*codeState:  0=NO EXITOSO, 1=EXITOSO, 2=no existe valor o vacio, 3=ya existe valor */
     )
 AS
 BEGIN
@@ -16,44 +17,53 @@ DECLARE
     @VNcount int,
     @VNid_User int;
     set @VNcount = 0;
-    set @msjTemp=' ';
+    set @pcMsj=' ';
 
 /* Validation of fields EMPTY when ADD user*/
 
             IF @name='  ' OR @name IS NULL BEGIN 
-                SET @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
+                
                 RETURN;   
             END;  
             IF @lastname=' ' OR @lastname IS NULL BEGIN
-                SET @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;     
             END;               
             IF @phone =' ' OR @phone IS NULL BEGIN
-                SET @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
             IF @email=' ' OR @email IS NULL BEGIN
-                SET @msjTemp='EMPTY FIELD';
+                SET @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;     
             END;         
             IF @password=' ' OR @password IS NULL BEGIN 
-                set @msjTemp='EMPTY FIELD';
+                set @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
             IF @address=' ' OR @address IS NULL BEGIN 
-                set @msjTemp='EMPTY FIELD';
+                set @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
            
             IF @register_id=' ' OR @register_id IS NULL BEGIN 
-                set @msjTemp='EMPTY FIELD';
+                set @pcMsj='EMPTY FIELD';
+                SET @CodeState=2;
                 RETURN;
             END;
 
             SET @VNcount = (SELECT count(*)  FROM TBL_INDIVIDUAL_CLIENTS
             where register_id=@register_id);
             IF @VNcount>0 BEGIN
-                set @msjTemp='Existing User';
+                SET @pcMsj='Existing User';
+                SET @CodeState=3;
                 RETURN;
             END;
             
@@ -64,12 +74,20 @@ DECLARE
             INSERT INTO TBL_INDIVIDUAL_CLIENTS(birth_date,register_id,idUser) 
             VALUES (cast(@birth_date as date),@register_id,@VNid_User);
             
-            SET @msjTemp='Properly Registered User';
-            set @id_user=@VNid_User;
-
+             IF  @VNid_User IS NULL BEGIN
+                SET @pcMsj='User Registration Error';
+                SET @CodeState=0;
+                RETURN;
+            END
+            ELSE BEGIN 
+                SET @pcMsj='Properly Registered User';
+                SET @id_user=@VNid_User;
+                SET @CodeState=1;
+            END;
 END;
 
-drop procedure SP_ADD_USER_INDIVIDUAL
+
+/*drop procedure SP_ADD_USER_INDIVIDUAL
 
 exec SP_ADD_USER_INDIVIDUAL 'estef@.com','pajaritos','658',' colonia','ledys','santos','2018-02-02','0801','bien','1';
 
@@ -78,6 +96,4 @@ delete from TBL_INDIVIDUAL_CLIENTS where idUser=15;
 
 select * from TBL_INDIVIDUAL_CLIENTS
 
-select * from TBL_USERS
-
-
+select * from TBL_USERS*/
