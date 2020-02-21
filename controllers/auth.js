f81b350f05667734f677999eb798e0012e63b9ec
Misael Landero <misalande@gmail.com>
2020-02-21 17:18:29 -0600

    login params validation, and error responses

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,11 +4,18 @@ const mssql = require("mssql");
 const crypto = require("crypto");
 const moment = require("moment");
 const sendEmail = require("../utils/sendEmail.js");
+const { validationResult } = require("express-validator");
+const errorResponse = require("../utils/errorResponse");
 
 //@desc     AUTH user
 //@route    GET     /api/auth/login
 //@access   Public
 exports.login = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return errorResponse(400, "Validaton errors", errors.array(), res);
+  }
+
   let payload = {};
   const { email, password } = req.body;
   const { ADMIN_PASSWORD, ADMIN_EMAIL } = process.env;
@@ -30,10 +37,12 @@ exports.login = async (req, res, next) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(404).json({
-        success: false,
-        msg: "Invalid credentials"
-      });
+      return errorResponse(
+        400,
+        "Invalid credentials",
+        [{ msg: "Email && password dont match" }],
+        res
+      );
     }
 
     query = await new mssql.Request()
@@ -184,7 +193,7 @@ const getUser = async (email, res) => {
   if (!dataExist) {
     return res.status(404).json({
       success: false,
-      msg: "Invalid credentials"
+      msg: "Invalid credentials, cant find user"
     });
   }
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 
 const { login, forgotPassword, resetPassword } = require("../controllers/auth");
 
-router.route("/login").post(login);
+router
+  .route("/login")
+  .post(
+    [
+      check("email", "please enter a valid email").isEmail(),
+      check(
+        "password",
+        "please enter a password of 8 character minimun"
+      ).isLength({ min: 8 })
+    ],
+    login
+  );
 router.route("/forgotpassword").post(forgotPassword);
 router.route("/resetpassword/:token").put(resetPassword);
 
