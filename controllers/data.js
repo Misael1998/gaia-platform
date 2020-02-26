65efcdbffa92151abce505a460055161bd98c154
Jorge Rivera <jorgeman201@yahoo.com>
2020-02-26 00:18:35 -0600

    endpoint funcionando con la base de datos, por el momento hice la petición enduro,empezaré a crear el SP correspondiente

--- controllers/data.js
+++ controllers/data.js
@@ -1,4 +1,11 @@
 const mssql = require("mssql");
+
+
+const {
+  validationResult
+} = require("express-validator");
+const errorResponse = require("../utils/errorResponse");
+
 //@desc     database public data
 //@route    GET     /api/data/sectors
 //@access   Public
@@ -27,6 +34,35 @@ exports.sectors = async (req, res, next) => {
     return res.status(500).json({
       success: false,
       msg: "sever error"
+    });
+  }
+};
+
+//@desc     get supplies
+//@route    GET     /api/data/supplies
+//@access   Private
+exports.supplies = async (req, res, next) => {
+  try {
+    const request = await new mssql.Request().query("SELECT * FROM TBL_SUPPLIES");
+    const data = request.recordset;
+    if (data.length == 0) {
+      return res.status(200).json({
+        success: true,
+        msg: "There are no supplies.",
+        data
+      })
+    }else{
+      return res.status(200).json({
+        success:true,
+        msg: "supplies data",
+        data
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      succes: false,
+      msg: "Server error"
     });
   }
 };
--- routes/data.js
+++ routes/data.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const { sectors } = require("../controllers/data");
+const { sectors,supplies } = require("../controllers/data");
 
+
+router.route("/supplies").get(supplies);
 router.route("/sectors").get(sectors);
 
 module.exports = router;
