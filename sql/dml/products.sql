7cfb5dcaa623d9698f6159c6d15cf3384f9096c3
Misael Landero <misalande@gmail.com>
2020-02-29 17:27:21 -0600

    inicio de creacion de escripts de db

---
+++ sql/dml/products.sql
@@ -0,0 +1,78 @@
+-- Insert rows into table 'TBL_PRODUCTS' in schema '[dbo]'
+INSERT INTO [pyflor].[dbo].[TBL_PRODUCTS]
+( -- Columns to insert data into
+ [name], [description], [idSarTypes]
+)
+VALUES
+( 
+ 'albahaca criolla', 
+ 'albahaca con sabor amargo para poder preparar tus comidas y tes', 
+ 2
+),
+( 
+ 'albahaca dulce', 
+ 'albahaca con sabor dulce para decorar tus comidas y darle nuevo sabor a tus tes', 
+ 1
+),
+( 
+ 'albahaca en bolsa', 
+ 'albahaca lista para sembrar donde tu desees', 
+ 2
+),
+( 
+ 'albahaca limon', 
+ 'albahaca preferida para combinar con jugos naturales', 
+ 1
+),
+( 
+ 'albahaca morada', 
+ 'albahaca con sabor amargo para poder preparar tus comidas y tes', 
+ 1
+),
+( 
+ 'albahaca thai', 
+ 'variedad de albahaca con hojas mas alargadas y pequeñas de sabor mas fuerte que el normal y usada en recetas con pollo y carnes blancas', 
+ 1
+),
+( 
+ 'arugula', 
+ 'tipo de hortaliza especialmente utilizada para ensaladas. Tambien se puede utilizar como verdura en las pastas.', 
+ 1
+),
+( 
+ 'bolsa de lechuga', 
+ 'conjunto de lechugas para poder preparar tus comidas', 
+ 2
+),
+( 
+ 'chive', 
+ 'hierba con sabor parecido a la cebolla blanca utilizado mayormente como hierba aromatica.', 
+ 2
+),
+( 
+ 'eneldo', 
+ 'hierba aromatica utilizada como condimento o para hacer infusiones.', 
+ 1
+),
+( 
+ 'escarola amarilla', 
+ 'tipo de lechuga de color amarillo con sabor picante y ligeramente amargo', 
+ 2
+),
+( 
+ 'escarola romana', 
+ 'tipo de lechuga con hojas alargadas y poco mas crujientes.', 
+ 2
+),
+( 
+ 'escarola verde', 
+ 'tipo de lechuga de color verde con sabor picante y ligeramente amargo', 
+ 2
+),
+( 
+ 'espinaca', 
+ 'planta con hojas comestibles grandes y de color verde muy oscuro. Se puede consumir fresca, cocida o frita.', 
+ 2
+)
+
+GO
