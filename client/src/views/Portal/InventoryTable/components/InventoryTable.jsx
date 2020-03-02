06a4896db2249f9fb26f45d2fe0e2bb825e73684
Rony-R <36996788+Rony-R@users.noreply.github.com>
2020-03-01 20:48:02 -0600

    agregando la informacion de la BD a la tabla

--- client/src/views/Portal/InventoryTable/components/InventoryTable.jsx
+++ client/src/views/Portal/InventoryTable/components/InventoryTable.jsx
@@ -1,44 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Spinner from "../../../../components/Spinner";
+import { getInventory } from "../../../../services/Inventory";
 
 const InventoryTable = () => {
-  return (
-    <div className="">
-      <table className="table table-bordered table-striped">
-        <thead className="primary-color text-white">
-          <tr>
-            <th scope="col">N° de Orden</th>
-            <th scope="col">Nombre Insumo</th>
-            <th scope="col">Precio Unitario</th>
-            <th scope="col">Cantidad Comprada</th>
-            <th scope="col">Excento de Impuestos</th>
-            <th scope="col">Fecha Entrada</th>
-            <th scope="col">Empleado que Recibio</th>
-          </tr>
-        </thead>
-        <tbody className="">
-          <tr className="">
-            <th scope="row">1</th>
-            <td>Vodka</td>
-            <td>900</td>
-            <td>8 botellas</td>
-            <td>Simon</td>
-            <td>12/01/2020</td>
-            <td>Rony</td>
-          </tr>
+  //State para el inventario:
+  const [regInventory, handleRegInventory] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-          <tr className="">
-            <th scope="row">2</th>
-            <td>Miller Draft</td>
-            <td>30</td>
-            <td>8 sixs</td>
-            <td>Nell prro</td>
-            <td>25/21/2020</td>
-            <td>SumaPichas(David)</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+  //Funcion para traer la info del inventario:
+  useEffect(() => {
+    getInventory()
+      .then(res => {
+        handleRegInventory(res);
+        setLoading(false);
+      })
+      .catch(err => console.log("El error es:", err));
+  }, []);
+
+  if (loading) {
+    return <Spinner />;
+  } else {
+    return (
+      <div className="">
+        <table className="table table-bordered table-striped">
+          <thead className="primary-color text-white">
+            <tr>
+              <th scope="col">N° de Orden</th>
+              <th scope="col">Nombre Insumo</th>
+              <th scope="col">Precio Unitario</th>
+              <th scope="col">Cantidad Comprada</th>
+              <th scope="col">Excento de Impuestos</th>
+              <th scope="col">Fecha Entrada</th>
+              <th scope="col">Empleado que Recibio</th>
+            </tr>
+          </thead>
+          <tbody className="">
+            {regInventory.map(registro => (
+              <tr key={registro.No_Orden} className="">
+                <th scope="row">{registro.No_Orden}</th>
+                <td>{registro.Supplie_Name}</td>
+                <td>{registro.unit_price}</td>
+                <td>{registro.quantity}</td>
+                <td></td>
+                <td>{registro.emission_date}</td>
+                <td>{registro.Receiver_Employee}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
 
 export default InventoryTable;
