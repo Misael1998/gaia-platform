3746a009585a430f9705dbf2229626f2c25ff6e5
Ariadna Perez <ariadna.perez97@yahoo.com>
2020-03-03 11:49:51 -0600

    insert de la tabla tbl_employees

---
+++ sql/dml/departments.sql
@@ -0,0 +1,14 @@
+-- Insert rows into table '' in schema '[dbo]'
+INSERT INTO [pyflor].[dbo].[TBL_DEPARTMENTS]
+( -- Columns to insert data into
+ [name]
+)
+VALUES
+(
+ 'administración'
+),
+(
+ 'Mercadeo y Ventas'
+)
+-- Add more rows here
+GO
