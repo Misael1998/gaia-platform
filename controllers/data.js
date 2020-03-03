fbd172813ceaab8c13d7cd1ff1cfe9a58a68540f
Misael Landero <misalande@gmail.com>
2020-03-03 16:54:02 -0600

    fixing conflict conflicts

--- controllers/data.js
+++ controllers/data.js
@@ -1,6 +1,4 @@
 const mssql = require("mssql");
-const errorResponse = require("../utils/errorResponse");
-
 //@desc     database public data
 //@route    GET     /api/data/sectors
 //@access   Public
@@ -127,6 +125,126 @@ exports.sar_type = async (req, res, next) => {
   }
 };
 
+//@desc     database all products
+//@route    GET     /api/data/products
+//@access   Private
+exports.products = async (req, res) => {
+  //console.log(req.url);
+  try {
+    const query = await new mssql.Request().query(
+      "SELECT idProducts, " +
+        "productName, " +
+        "productImage, " +
+        "productDescription, " +
+        "category, " +
+        "sarType, " +
+        "companyType, " +
+        "unit_price " +
+        "FROM [dbo].[FT_GET_ALL_PRODUCTS_DATA]();"
+    );
+    const data = query.recordset;
+    if (data.length == 0) {
+      return res.status(200).json({
+        success: true,
+        msg: "No data",
+        data
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      msg: "products data",
+      data
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      msg: "server error"
+    });
+  }
+};
+
+//@desc     database single products
+//@route    GET     /api/data/products
+//@access   Private
+exports.individualProduct = async (req, res) => {
+  /*res.send(`id: ${req.params.id}`)*/
+  const idProduct = req.params.id;
+  try {
+    const query = await new mssql.Request()
+      .input("id", mssql.Int, idProduct)
+      .query(
+        "SELECT idProducts, " +
+          "productName, " +
+          "productImage, " +
+          "productDescription, " +
+          "category, " +
+          "sarType, " +
+          "companyType, " +
+          "unit_price " +
+          "FROM [dbo].[FT_GET_SINGLE_PRODUCT_DATA](@id);"
+      );
+    const data = query.recordset;
+    if (data.length == 0) {
+      return res.status(200).json({
+        success: true,
+        msg: "No data ",
+        data
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      msg: "product data",
+      data
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      msg: "server error"
+    });
+  }
+};
+
+//@desc     database all employees
+//@route    GET     /api/data/employees
+//@access   Private
+exports.employees = async (req, res) => {
+  try {
+    const query = await new mssql.Request().query(
+      "SELECT idEmployees, " +
+        "userName, " +
+        "lastname, " +
+        "jobTitle, " +
+        "departmentName, " +
+        "admission_date, " +
+        "email, " +
+        "phone, " +
+        "address " +
+        "FROM [dbo].[FT_GET_EMPLOYEES_DATA]();"
+    );
+    const data = query.recordset;
+    if (data.length == 0) {
+      return res.status(200).json({
+        success: true,
+        msg: "No data",
+        data
+      });
+    }
+    return res.status(200).json({
+      success: true,
+      msg: "employees data",
+      data
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      msg: "server error"
+    });
+  }
+};
+
 //@desc     get all providers
 //@route    GET     /api/data/providers
 //@access   Private
@@ -195,114 +313,5 @@ exports.getProvider = async (req, res, next) => {
       [{ msg: "internal server error" }],
       res
     );
-  }
-};
-
-//@desc     database all products
-//@route    GET     /api/data/products
-//@access   Private
-exports.products = async (req, res) => {
-  //console.log(req.url);
-  try {
-    const query = await new mssql.Request().query("SELECT * FROM TBL_PRODUCTS");
-    const data = query.recordset;
-    if (data.length == 0) {
-      return res.status(200).json({
-        success: true,
-        msg: "There are no products",
-        data
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      msg: "products data",
-      data
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false,
-      msg: "server error"
-    });
-  }
-};
-
-//@desc     database single products
-//@route    GET     /api/data/products
-//@access   Private
-exports.individualProduct = async (req, res) => {
-  /*res.send(`id: ${req.params.id}`)*/
-  const idProduct = req.params.id;
-  try {
-    const query = await new mssql.Request().query(
-      "SELECT idEmployees, " +
-        "userName, " +
-        "lastname, " +
-        "jobTitle, " +
-        "departmentName, " +
-        "admission_date, " +
-        "email, " +
-        "phone, " +
-        "address" +
-        "FROM [dbo].[FT_GET_EMPLOYEES_DATA]();"
-    );
-    const data = query.recordset;
-    if (data.length == 0) {
-      return res.status(200).json({
-        success: true,
-        msg: "No data",
-        data
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      msg: "employees data",
-      data
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false,
-      msg: "server error"
-    });
-  }
-};
-
-//@desc     database all employees
-//@route    GET     /api/data/employees
-//@access   Private
-exports.employees = async (req, res) => {
-  try {
-    const query = await new mssql.Request().query(
-      "SELECT idEmployees, " +
-        "userName, " +
-        "lastname, " +
-        "jobTitle, " +
-        "departmentName, " +
-        "admission_date, " +
-        "email, " +
-        "phone, " +
-        "address" +
-        "FROM [dbo].[FT_GET_EMPLOYEES_DATA]();"
-    );
-    const data = query.recordset;
-    if (data.length == 0) {
-      return res.status(200).json({
-        success: true,
-        msg: "No data",
-        data
-      });
-    }
-    return res.status(200).json({
-      success: true,
-      msg: "employees data",
-      data
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      success: false,
-      msg: "server error"
-    });
   }
 };
