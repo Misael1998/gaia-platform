e493a58b839266fdebfe0a492b6b12a6e77898c3
ledys.santos <estefany.santos129@gmial.com>
2020-03-03 03:21:41 -0600

    Corregido underscore y  error 500 a 404

--- routes/data.js
+++ routes/data.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { sectors, sar_type,inventory } = require("../controllers/data");
+/*const { sectors, , } = require("../controllers/data");*/
 
 
 
 
 const isLogged = require('../middleware/data');
 
-const { sectors,supplies,individualProduct,products } = require("../controllers/data");
+const { sectors,supplies,individualProduct,products,sarType,inventory} = require("../controllers/data");
 
 router.route("/supplies").get(isLogged,supplies);
 router.route("/sectors").get(sectors);
-router.route("/sar_type").get(sar_type);
+router.route("/sarType").get(sarType);
 router.route("/inventory").get(inventory);
 router.route("/products").get(products);
 router.route("/products/:id").get(individualProduct);
