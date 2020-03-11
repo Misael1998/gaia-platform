3649b65bc2af05949742e3197d0882951154f7a1
Jorge Rivera <jorgeman201@yahoo.com>
2020-03-11 13:49:56 -0600

    Agregado los campos emission_date y expire_date a la respuesta del endpoint,también agregé los apellidos a los nombres de los empleados que están relacionados con la orden

--- sql/functions/FT_REFFERALS.sql
+++ sql/functions/FT_REFFERALS.sql
@@ -1,15 +1,19 @@
-create FUNCTION FT_REFFERALS()
+IF OBJECT_ID (N'FT_REFFERALS', N'IF') IS NOT NULL  
+    DROP FUNCTION FT_REFFERALS;  
+GO  
+CREATE FUNCTION FT_REFFERALS()
 RETURNS TABLE
 AS
 RETURN
     SELECT 
         r.idRefferals idRefferal,
         r.idOrders as idOrder,
-        o.idReceiverEmployee,
-        u.name as CreatedEmployee,
-        uu.name as SenderEmployee,
-        uuu.name as ReceiverEmployee,
-        uuuu.name as AddresseeEmployee
+        o.expired_date,
+        o.emission_date,
+        userCreated.name + ' '+ userCreated.lastname as CreatedEmployee,
+        senderUser.name + ' '+senderUser.lastname as SenderEmployee,
+        receiverUser.name + ' '+receiverUser.lastname as ReceiverEmployee,
+        addresseeUser.name +' '+addresseeUser.lastname as AddresseeEmployee
     FROM
         TBL_REFFERALS as r
     INNER JOIN 
@@ -17,34 +21,34 @@ RETURN
     ON 
         o.idOrders=r.idOrders
     INNER JOIN 
-        TBL_EMPLOYEES as e
+        TBL_EMPLOYEES as createdEmployee
     ON 
-        o.idEmployees=e.idEmployees
+        o.idEmployees=createdEmployee.idEmployees
     INNER JOIN 
-        TBL_EMPLOYEES as ee
+        TBL_EMPLOYEES as senderEmployee
     on 
-        o.idSenderEmployee=ee.idEmployees
+        o.idSenderEmployee=senderEmployee.idEmployees
     INNER JOIN 
-        TBL_EMPLOYEES as eee
+        TBL_EMPLOYEES as receiverEmployee
     on 
-        o.idReceiverEmployee=eee.idEmployees
+        o.idReceiverEmployee=receiverEmployee.idEmployees
     INNER JOIN 
-        TBL_EMPLOYEES as eeee
+        TBL_EMPLOYEES as addresseeEmployee
     on 
-        o.idAddresseeEmployee=eeee.idEmployees
+        o.idAddresseeEmployee=addresseeEmployee.idEmployees
     INNER JOIN 
-        TBL_USERS AS u
+        TBL_USERS AS userCreated
     ON 
-        u.idUser = e.idUser
+        userCreated.idUser = createdEmployee.idUser
     INNER JOIN 
-        TBL_USERS AS uu
+        TBL_USERS AS senderUser
     on 
-        uu.idUser = ee.idUser
+        senderUser.idUser = senderEmployee.idUser
     INNER JOIN 
-        TBL_USERS AS uuu
+        TBL_USERS AS receiverUser
     on 
-        uuu.idUser = eee.idUser
+        receiverUser.idUser = receiverEmployee.idUser
     INNER JOIN 
-        TBL_USERS AS uuuu
+        TBL_USERS AS addresseeUser
     on 
-        uuuu.idUser = eeee.idUser
+        addresseeUser.idUser = addresseeEmployee.idEmployees
