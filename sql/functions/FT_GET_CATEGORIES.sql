021c14106fbc357ff6da5ddbdc05925d53af17c5
Misael Landero <misalande@gmail.com>
2020-04-03 22:55:52 -0600

    ruta para obtener las categorias

---
+++ sql/functions/FT_GET_CATEGORIES.sql
@@ -0,0 +1,11 @@
+IF OBJECT_ID (N'FT_GET_CATEGORIES', N'IF') IS NOT NULL
+    DROP FUNCTION [FT_GET_CATEGORIES];
+GO
+CREATE FUNCTION [FT_GET_CATEGORIES] ()
+RETURNS TABLE
+AS  
+RETURN(
+    SELECT  idCategories as id,
+            name as name
+    FROM [pyflor].[dbo].[TBL_CATEGORIES]
+)
