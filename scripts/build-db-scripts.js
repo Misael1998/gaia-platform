8779d202341d7642224073e65d17d4f1f918daf2
Misael Landero <misalande@gmail.com>
2020-04-14 05:35:21 -0600

    creando script para generar un archivo unificado con funciones y sp

---
+++ scripts/build-db-scripts.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+
+const fnPath = path.join(__dirname, "../sql/functions");
+const spPath = path.join(__dirname, "../sql/sp");
+
+fs.readdir(spPath, (err, files) => {
+  if (err) {
+    console.log("something went wrong");
+    process.exit(1);
+  }
+  let unified = "";
+  for (file of files) {
+    const tmp = fs.readFileSync(`${spPath}/${file}`);
+    unified += `${tmp}\n`;
+  }
+
+  const writeStream = fs.createWriteStream(
+    path.join(__dirname, "../sql/build/sp.sql")
+  );
+
+  writeStream.write(unified);
+});
+
+fs.readdir(fnPath, (err, files) => {
+  if (err) {
+    console.log("something went wrong");
+    process.exit(1);
+  }
+  let unified = "";
+  for (file of files) {
+    const tmp = fs.readFileSync(`${fnPath}/${file}`);
+    unified += `${tmp}\n`;
+  }
+
+  const writeStream = fs.createWriteStream(
+    path.join(__dirname, "../sql/build/fn.sql")
+  );
+
+  writeStream.write(unified);
+});
