14363aea471710a301caa0a4224d61d83c76c47e
Sumachik <davidflores.irias@gmail.com>
2020-04-29 02:07:04 -0600

    maquetando pagina de inicio de admin

--- client/src/views/Admin/Admin.js
+++ client/src/views/Admin/Admin.js
@@ -5,6 +5,12 @@ import items from "../../constants/adminNavigation";
 import FormRegEmp from "./FormEmployee/components/FormRegEmp";
 import FormProduct from "./FormProduct/FormProduct";
 
+import Title from "../../components/Title";
+
+import { FaUserAlt } from "react-icons/fa";
+
+import { welcomeMessage } from "../../modules/helper";
+
 const Portal = ({ match }) => {
   return (
     <div className="row">
@@ -22,6 +28,30 @@ const Portal = ({ match }) => {
           component={FormProduct}
           isPrivate
         />
+
+        <div className="row p-5">
+          <Title title="Bienvenido a PYFLOR" icon={<FaUserAlt size={40} />} />
+
+          <div className="col-12">
+            <div className="row">
+              <div className="col-12 font-weight-bold text-center mb-3">
+                <p className="font-xl">{welcomeMessage()}</p>
+
+                <p className="font-xl">
+                  ¡Bienvenido {sessionStorage.getItem("uName")}!
+                </p>
+              </div>
+
+              <div className="col-lg-12 col-md-12 text-center p-2">
+                <img
+                  src={require("../../assets/img/header.jpg")}
+                  alt="Tienda"
+                  className="img-fluid div-radius"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
