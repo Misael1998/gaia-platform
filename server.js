dc258220aa14e448b39a9a90d0de4c4a9be3ac5b
Ariadna Perez <ariadna.perez97@yahoo.com>
2020-05-03 15:33:41 -0600

    register new provider

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const request = require("./routes/request");
 const employees = require("./routes/employees");
 const payment = require("./routes/payment");
 const product = require("./routes/product");
+const provider = require("./routes/provider");
 
 //mount routes
 server.use("/api/auth", auth);
@@ -29,6 +30,7 @@ server.use("/api/request", request);
 server.use("/api/employees", employees);
 server.use("/api/payment", payment);
 server.use("/api/product", product);
+server.use("/api/provider", provider);
 
 //init database
 db();
@@ -37,21 +39,21 @@ db();
 //@route    GET     /
 //@access   Public
 if (process.env.NODE_ENV === "development") {
-  server.get("/", (req, res) => {
-    res.send("gaia-pyflor");
-  });
+    server.get("/", (req, res) => {
+        res.send("gaia-pyflor");
+    });
 }
 // Serve static assets in production
 if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "sprint"
+    process.env.NODE_ENV === "production" ||
+    process.env.NODE_ENV === "sprint"
 ) {
-  // Set static folder
-  server.use(express.static("client/build"));
+    // Set static folder
+    server.use(express.static("client/build"));
 
-  server.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+    server.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
 }
 
 //setting port
@@ -59,5 +61,5 @@ const PORT = process.env.PORT || 5000;
 
 //expxose port to server to listend
 server.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
