c0e4e1d52be2ba66efbd65d3e361ea80bc3d4a73
Rony-R <ronyrodriguez2011@hotmail.com>
2020-05-17 19:42:55 -0600

    Haciendo responsive el carrito

--- client/src/views/Main/components/Cartg.jsx
+++ client/src/views/Main/components/Cartg.jsx
@@ -1,74 +1,69 @@
-import React from 'react'
+import React from "react";
 import { MdShoppingCart } from "react-icons/md";
-import { useSelector } from 'react-redux'
+import { useSelector } from "react-redux";
 import CartItem from "../components/CartItem";
 import Title from "../../../components/Title";
 import { Link } from "react-router-dom";
 
-
 const Cartg = ({ match }) => {
+  const cart = useSelector((state) => state.cart.cart);
+  let sum = 0,
+    isv = 0,
+    sumExcent = 0,
+    sumGrav = 0;
 
-  const cart = useSelector(state => state.cart.cart);
-  let sum = 0, isv = 0, sumExcent = 0, sumGrav = 0;
-
-  cart.forEach(product => {
-    sum += product.unit_price * product.quantity
-    if (product.sarType === 'E') {
+  cart.forEach((product) => {
+    sum += product.unit_price * product.quantity;
+    if (product.sarType === "E") {
       sumExcent += product.unit_price * product.quantity;
     } else {
       sumGrav += product.unit_price * product.quantity;
     }
-  })
+  });
   sum = sum.toFixed(2);
   sumExcent = sumExcent.toFixed(2);
   sumGrav = sumGrav.toFixed(2);
   isv = (sum * 0.15).toFixed(2);
   let total = (Number(sum) + Number(isv)).toFixed(2);
 
-
   if (cart.length === 0) {
     return (
-      <div className='row p-5'>
+      <div className="row p-5">
         <Title icon={<MdShoppingCart size={40} />} title="Carrito" />
-        <div className='col-12'>
+        <div className="col-12">
           <h4>No hay productos en el carrito actualmente</h4>
         </div>
       </div>
-    )
+    );
   } else {
-
     return (
-
       <div className="row p-5">
         <Title icon={<MdShoppingCart size={40} />} title="Carrito" />
-        <div className="col-8">
-          {
-            cart.map(product => (
-              <CartItem key={product.idProducts} product={product} />
-            ))
-          }
+        <div className="col-lg-8 col-md-8 col-sm-12">
+          {cart.map((product) => (
+            <CartItem key={product.idProducts} product={product} />
+          ))}
         </div>
-        <div className="col-4">
+        <div className="col-lg-4 col-md-4 col-sm-12">
           <div className="mb-4">
             <h4 className="text-center">Resumen de compra</h4>
           </div>
           <div className="mb-2">
-            <span className="font-weight-bold m-r-170">Subtotal:</span>L
-                {sum}
+            <span className="font-weight-bold m-r-170">Subtotal:</span>L{sum}
           </div>
           <div className="mb-2">
             <span className="font-weight-bold m-r-173">ISV 15%:</span>L {isv}
           </div>
           <div className="mb-2">
             <span className="font-weight-bold m-r-173">ISV 18%:</span>L 0.00
-              </div>
+          </div>
           <div className="mb-2">
             <span className="font-weight-bold m-r-135">Total Excento:</span>L
-                {sumExcent}
+            {sumExcent}
           </div>
           <div className="mb-2">
             <span className="font-weight-bold m-r-130">Total Gravado:</span>L
-                {sumGrav}
+            {sumGrav}
           </div>
           <div>
             <hr />
@@ -77,17 +72,17 @@ const Cartg = ({ match }) => {
             <span className="font-weight-bold ">Total:</span> L {total}
           </div>
           <div className="mt-5">
-
-          <Link className="btn btn-success btn-block" to={`${match.path}/shipping`}>
+            <Link
+              className="btn btn-success btn-block"
+              to={`${match.path}/shipping`}
+            >
               Proceder con el pago
-          </Link>
-
+            </Link>
           </div>
         </div>
       </div>
-
     );
   }
-}
+};
 
 export default Cartg;
